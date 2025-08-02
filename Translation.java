5a95889ea188c51d534e219894456677d88808a7
Gladden <mr.gladdenpravin@gmail.com>
2025-08-02 22:27:25 +0530

    Day 7 - CodeForces ANS

---
+++ Translation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Translation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String t = scanner.nextLine();
+
+        if (s.length() != t.length()) {
+            System.out.println("NO");
+            return;
+        }
+        scanner.close();
+
+        String reversedS = new StringBuilder(s).reverse().toString();
+        if (reversedS.equals(t)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
