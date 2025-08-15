324ca586b85b4268e6867db6867c66bf3f57b4b5
Gladden <mr.gladdenpravin@gmail.com>
2025-08-15 22:48:40 +0530

    Day-19 Codeforces Solution

---
+++ BearAndBigBrother.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BearAndBigBrother {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int years = 0;
+
+        while (a <= b) {
+            a *= 3;
+            b *= 2;
+            years++;
+        }
+
+        System.out.println(years);
+        scanner.close();
+    }
+}
