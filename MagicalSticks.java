bcd9873c90f07120e1e8c829526225894bfda001
Gladden <mr.gladdenpravin@gmail.com>
2025-08-27 19:25:41 +0530

    Day -30 Codeforces solution

---
+++ MagicalSticks.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MagicalSticks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long m = (n + 1) / 2;
+            ;
+            System.out.println(m);
+        }
+
+        sc.close();
+    }
+}
