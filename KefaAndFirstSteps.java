b929afe7c1d99a15ddd1979ed432622a6a6f5f85
Gladden <mr.gladdenpravin@gmail.com>
2025-09-04 22:02:01 +0530

    Day-40 codeforces solution

---
+++ KefaAndFirstSteps.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class KefaAndFirstSteps {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if (n == 1) {
+            System.out.println(1);
+        }
+
+        int maxLength = 1;
+        int currentLength = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i + 1] >= arr[i]) {
+                currentLength++;
+            } else {
+                currentLength = 1;
+            }
+
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+            }
+        }
+
+        System.out.println(maxLength);
+        sc.close();
+    }
+}
