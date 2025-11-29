faee0b854e4eca13ce6865d31e2ebf6657505938
Gladden <mr.gladdenpravin@gmail.com>
2025-11-29 19:44:32 +0530

    Day-124 codeforces solution

---
+++ MaximumGCD.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MaximumGCD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt();
+            System.out.println(n / 2);
+        }
+
+        scanner.close();
+    }
+}
